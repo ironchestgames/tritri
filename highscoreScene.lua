765bde6ebd81ba9f2e365d01b3054eeb2c9fc121
Fredrik Järnbröst <contact@fredrikvestin.com>
2017-10-18 01:17:16 +0200

    bg animation in highscore scene

--- highscoreScene.lua
+++ highscoreScene.lua
@@ -1,18 +1,24 @@
 
+local anim8 = require('anim8')
 local jupiter = require('jupiter')
 local stateswitcher = require('stateswitcher')
 local vars = require('vars')
 
 local bgImage
+local blockAnimationImage
 local canvas
 local font
 
 local savedScores
 
+local backgroundPositions = {}
+local backgroundAnimations = {}
+
 function love.load()
 
   -- load images
   bgImage = love.graphics.newImage('art/highscore_layout.png')
+  blockAnimationImage = love.graphics.newImage('art/highscore_blockanim.png')
 
   -- load font
   font = love.graphics.newImageFont('art/font.png', vars.GLYPHS)
@@ -20,13 +26,32 @@ function love.load()
 
   canvas = love.graphics.newCanvas()
 
+  -- set background effect
   love.graphics.setBackgroundColor(155, 173, 183)
+
+  do
+    local widthInBlocks = (_G.SCREENWIDTH / _G.GRAPHICSSCALE) / 8 + 8
+    local heightInBlocks = (_G.SCREENHEIGHT / _G.GRAPHICSSCALE) / 8 + 8
+
+    for y = 0, heightInBlocks do
+      for x = 0, widthInBlocks do
+        local g = anim8.newGrid(8, 8, blockAnimationImage:getWidth(), blockAnimationImage:getHeight())
+        local animation = anim8.newAnimation(g('1-8', 1), 0.30 + love.math.random(1, 2) * 0.0016)
+        animation:update(0.08 * (y + 1) + 0.03 * x)
+        table.insert(backgroundAnimations, {
+          animation = animation,
+          x = x * 8 - 3,
+          y = y * 8 - 3,
+        })
+      end
+    end
+  end
 
   -- load scores
   savedScores = jupiter.load(vars.HIGHSCORE_FILE_NAME)
 
   if savedScores == nil then
-  	savedScores = {}
+    savedScores = {}
   end
 
 end
@@ -39,11 +64,24 @@ function love.keyreleased(key)
 end
 
 function love.update(dt)
+  for i, object in ipairs(backgroundAnimations) do
+    object.animation:update(dt)
+  end
 end
 
 function love.draw()
 
   love.graphics.setColor(255, 255, 255, 255)
+
+  love.graphics.push()
+
+  love.graphics.scale(_G.GRAPHICSSCALE, _G.GRAPHICSSCALE)
+
+  for i, object in ipairs(backgroundAnimations) do
+    object.animation:draw(blockAnimationImage, object.x, object.y)
+  end
+
+  love.graphics.pop()
 
   love.graphics.setCanvas(canvas)
 
@@ -54,11 +92,11 @@ function love.draw()
   love.graphics.setColor(vars.TEXT_COLOR_DARK)
 
   for i = 1, 10 do
-  	local v = 0
-  	if savedScores[i] then
-  		v = savedScores[i]
-  	end
-  	love.graphics.printf(
+    local v = 0
+    if savedScores[i] then
+      v = savedScores[i]
+    end
+    love.graphics.printf(
         v,
         55,
         60 + 13 * (i - 1),
